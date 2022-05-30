6110904f24dd3a1c0119092a6a1db61b1e89efa4
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2022-05-30 12:18:12 +0300

    2015/day5: Solved p1+p2

--- 2015/day5/index.js
+++ 2015/day5/index.js
@@ -1,34 +1,110 @@
-const input = require("fs").readFileSync("input.txt", "utf-8");
+const input = require("fs").readFileSync("input.txt", "utf-8").split("\n");
+input.pop();
 
 /*
+    Part 1:
+
     It contains at least three vowels
     It contains at least one letter that appears twice in a row (xx, abcdde (dd), or aabbccdd (aa, bb, cc, or dd))
     It does not contain the strings ab, cd, pq, or xy
 */
 
-function checkForBannedStings(str) {
-    const banned_strings = ["ab", "cd", "pq", "xy"];
+function part1_check(str) {
+    function checkForBannedStings(str) {
+        const banned_strings = ["ab", "cd", "pq", "xy"];
 
-    let local_good = true;
+        let good = true;
 
-    for (let i = 0; i < banned_strings.length; i++) {
-        if (str.indexOf(banned_strings[i]) > 0) {
-            local_good = false;
-            break;
+        for (let i = 0; i < banned_strings.length; i++) {
+            if (str.indexOf(banned_strings[i]) >= 0) {
+                good = false;
+                break;
+            }
         }
+
+        return good;
     }
 
-    return local_good;
+    function checkForVowels(str) {
+        let vowels = ["a", "e", "i", "o", "u"];
+        let matches = 0;
+
+        for (let i = 0; i < str.length; i++) {
+            const char = str[i];
+            if (vowels.includes(char)) matches++;
+        }
+
+        return matches >= 3;
+    }
+
+    function checkForDoubleLetters(str) {
+        let good = false;
+
+        for (let i = 0; i < str.length - 1; i++) {
+            if (str[i] == str[i + 1]) {
+                good = true;
+                break;
+            }
+        }
+
+        return good;
+    }
+
+    return checkForBannedStings(str) && checkForVowels(str) && checkForDoubleLetters(str);
 }
 
-const part1_check = (str) => {
-    const vowels = "aeiou".split("");
+/*
+    Part 2:
 
-    let good = true;
+    It contains a pair of any two letters that appears at least
+    twice in the string without overlapping, like xyxy (xy) or
+    aabcdefgaa (aa), but not like aaa (aa, but it overlaps).
 
-    checkForBannedStings(str);
+    It contains at least one letter which repeats with exactly one
+    letter between them, like xyx, abcdefeghi (efe), or even aaa.
+*/
 
-    return good;
-};
+function part2_check(str) {
+    // Could've been combined into a single for-loop,
+    // but I went with pretty code instead
 
-console.log(checkForBannedStings("xy") ? "OK" : "NG");
+    function checkForPairsOfLetters(str) {
+        let good = false;
+
+        for (let i = 0; i < str.length - 2; i++) {
+            if (str.match(new RegExp(str[i] + str[i + 1], "g")).length > 1) {
+                good = true;
+                break;
+            }
+        }
+
+        return good;
+    }
+
+    // Yes.
+    function checkForRepeatingLetterWithADelimiter(str) {
+        let good = false;
+
+        for (let i = 0; i < str.length - 2; i++) {
+            if (str[i] == str[i + 2]) {
+                good = true;
+                break;
+            }
+        }
+
+        return good;
+    }
+
+    return checkForPairsOfLetters(str) && checkForRepeatingLetterWithADelimiter(str);
+}
+
+let part1 = 0,
+    part2 = 0;
+
+for (let i = 0; i < input.length; i++) {
+    const str = input[i];
+    if (part1_check(str)) part1++;
+    if (part2_check(str)) part2++;
+}
+
+console.log(part1, part2);
