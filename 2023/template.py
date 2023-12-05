1a3b6b30f79976f341791d4f557116b566d7745c
Daniil Isakov <12859907+oplexz@users.noreply.github.com>
2023-12-05 14:33:53 +0300

    Add template

---
+++ 2023/template.py
@@ -0,0 +1,37 @@
+input = None
+
+with open('input.txt') as f:
+    input = f.read()
+
+lines = input.splitlines()
+
+
+# region part_one
+def part_one():
+    ans = None
+
+    for line in lines:
+        pass
+
+    return ans
+# endregion part_one
+
+
+# region part_two
+def part_two():
+    ans = None
+
+    for line in lines:
+        pass
+
+    return ans
+# endregion part_two
+
+
+def main():
+    print(f"Soltuion for part one: {part_one()}")
+    print(f"Soltuion for part two: {part_two()}")
+
+
+if __name__ == "__main__":
+    main()
